6c5223160bf823f62d68aa0a5361cff3b2c2fcc9
HuynhCuongg <huynhcuongvan2004@gmail.com>
2025-01-14 21:54:18 +0700

    Enemy

---
+++ Assets/_Scripts/Enemy/Enemy.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+    public GameObject Target;
+    protected NavMeshAgent agent;
+    private void Update()
+    {
+        
+    }
+}
