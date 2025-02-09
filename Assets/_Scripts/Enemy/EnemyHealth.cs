b93a229a807d30684cc0afb5fa95b5895e266e3d
TuanAnhKazuto <tuananhkazuto@gmail.com>
2025-02-09 22:14:37 +0700

    UI hp for Enemy

--- Assets/_Scripts/Enemy/EnemyHealth.cs
+++ Assets/_Scripts/Enemy/EnemyHealth.cs
@@ -1,10 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemyHealth : MonoBehaviour
 {
     public float heath = 50f;
+
+    [Tooltip("HpBar => 'Slider'")]
+    public Slider hpSlider;
+
+    private void Start()
+    {
+        hpSlider.maxValue = heath;
+        hpSlider.value = heath;
+    }
+
+    private void Update()
+    {
+        hpSlider.value = heath;
+    }
 
     public void TakeDamage (float amount)
     {
