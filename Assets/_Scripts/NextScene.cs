c3af53137eb93046db12adf8fd1124b690b3b7d6
TuanAnhKazuto <tuananhkazuto@gmail.com>
2025-02-15 23:12:51 +0700

    set up loading in open scene

--- Assets/_Scripts/NextScene.cs
+++ Assets/_Scripts/NextScene.cs
@@ -1,16 +1,11 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class DoorTrigger : MonoBehaviour
+public class DoorTrigger : LoadingLevel
 {
     // Hàm này sẽ được gọi khi Player đi vào vùng trigger của cửa
     private void OnTriggerEnter(Collider other)
     {
-        // Kiểm tra xem đối tượng va chạm có tag là "Player" không
-        if (other.CompareTag("Player"))
-        {
-            // Chuyển đến scene "Scene01"
-            SceneManager.LoadScene("Scene01");
-        }
+        LoadLevel();
     }
 }
