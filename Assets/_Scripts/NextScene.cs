1a9883f3d2d596229e96fea38736cac1ab523897
TuanAnhKazuto <tuananhkazuto@gmail.com>
2025-02-25 02:38:18 +0700

    Set up Scene open

--- Assets/_Scripts/NextScene.cs
+++ Assets/_Scripts/NextScene.cs
@@ -3,9 +3,50 @@ using UnityEngine.SceneManagement;
 
 public class DoorTrigger : LoadingLevel
 {
-    // Hàm này sẽ được gọi khi Player đi vào vùng trigger của cửa
+    public LowPickUpItem lowPickUpItem;
+    public GameObject information;
+
+    public Animator inforAnim;
+
+    private void Start()
+    {
+        information.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (!lowPickUpItem.hasKey)
+        {
+            information.SetActive(true);
+            float time = inforAnim.GetCurrentAnimatorStateInfo(0).length;
+            if (time >= 1)
+            {
+                information.SetActive(false);
+            }
+        }
+    }
+
     private void OnTriggerEnter(Collider other)
     {
-        LoadLevel();
+        if(other.CompareTag("Player"))
+        {
+            lowPickUpItem.fKey.SetActive(true);
+
+            if (lowPickUpItem.hasKey)
+            {
+                if (Input.GetKeyDown(KeyCode.F))
+                {
+                    LoadLevel();
+                }
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            lowPickUpItem.fKey.SetActive(false);
+        }
     }
 }
