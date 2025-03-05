c845a3e3ca03a1d22ed11eca08b9a078b42d36a1
HAUMEOOOO <haumdps40975@gmail.com>
2025-03-05 23:01:28 +0700

    Hau
    update Scene

---
+++ Assets/Qua.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.SceneManagement;
+
+public class VideoEndSceneChange : MonoBehaviour
+{
+    public VideoPlayer videoPlayer; // Gán VideoPlayer trong Inspector
+    public string nextSceneName = "SceneOpen"; // Đặt tên Scene cần chuyển
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) // Khi nhấn phím Space
+        {
+            SceneManager.LoadScene(nextSceneName); // Chuyển Scene
+        }
+    }
+}
