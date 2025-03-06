dcb242bd68b2274dcc4bf75673b7325fbf740c93
TuanAnhKazuto <tuananhkazuto@gmail.com>
2025-03-06 10:32:14 +0700

    Update scene01

--- Assets/_Scripts/NextScene.cs
+++ Assets/_Scripts/NextScene.cs
@@ -7,28 +7,6 @@ public class DoorTrigger : LoadingLevel
     {
         base.Start();
     }
-    //public LowPickUpItem lowPickUpItem;
-    //public GameObject information;
-
-    //public Animator inforAnim;
-
-    //private void Start()
-    //{
-    //    information.SetActive(false);
-    //}
-
-    //private void Update()
-    //{
-    //    if (!lowPickUpItem.hasKey)
-    //    {
-    //        information.SetActive(true);
-    //        float time = inforAnim.GetCurrentAnimatorStateInfo(0).length;
-    //        if (time >= 1)
-    //        {
-    //            information.SetActive(false);
-    //        }
-    //    }
-    //}
 
     private void OnTriggerEnter(Collider other)
     {
