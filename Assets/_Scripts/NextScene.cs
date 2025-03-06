c50a5df0bd03c77dc3b1950662ba5d6b85d410e7
TuanAnhKazuto <tuananhkazuto@gmail.com>
2025-03-06 12:02:26 +0700

    lấy lại main từ tay thằng Ma Đức Mòe

--- Assets/_Scripts/NextScene.cs
+++ Assets/_Scripts/NextScene.cs
@@ -1,18 +1,51 @@
 ﻿using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class DoorTrigger : LoadingLevel
 {
+    FkeyFunction fkeyFunction;
+    public GameObject information;
+
+    private void Awake()
+    {
+        fkeyFunction = GameObject.Find("GameManager").GetComponent<FkeyFunction>();
+    }
     protected override void Start()
     {
         base.Start();
+        information.SetActive(false);
     }
 
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerStay(Collider other)
     {
-        if(other.gameObject.CompareTag("Player"))
+        if (other.gameObject.CompareTag("Player"))
         {
-            LoadLevel();
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                if (fkeyFunction.isHasKey)
+                {
+                    LoadLevel();
+                }
+                else
+                {
+                    Debug.Log("You need a key to open this door");
+                    information.SetActive(true);
+                    Invoke(nameof(CloseInformation), 1f);
+                }
+            }
+
+        }
+    }
+
+    public void CloseInformation()
+    {
+        information.SetActive(false);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            fkeyFunction.HideFkeyBase();
         }
     }
 }
