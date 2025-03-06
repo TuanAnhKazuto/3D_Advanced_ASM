c981998de98661fa187823fbd42806014ea2b4b2
TuanAnhKazuto <tuananhkazuto@gmail.com>
2025-03-06 13:52:09 +0700

    setup scene01

--- Assets/driver.cs
+++ Assets/driver.cs
@@ -2,17 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class driver : MonoBehaviour
+public class driver : LoadingLevel
 {
-    // Start is called before the first frame update
-    void Start()
+    protected override void Start()
     {
-        
+        base.Start();
     }
 
-    // Update is called once per frame
-    void Update()
+
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.gameObject.CompareTag("Player"))
+        {
+            LoadLevel();
+        }
     }
 }
